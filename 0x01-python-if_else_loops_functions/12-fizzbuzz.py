d7062748a4e18c892306dc4218fc205ddfa35286
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-12-03 12:42:53 +0200

    Updated

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1:100):
-        if i == i % 3:
-            print("Fizz")
-        elif i == i % 5:
-            print("Buzz")
-        elif i == i % 3 and i == i % 5:
-            print("FizzBuzz")
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end="")
+        elif i % 5 == 0:
+            print("Buzz", end="")
+        elif i % 15 == 0:
+            print("FizzBuzz", end="")
         else:
-            print({":d"}.format(i), end="")
+            print("{:d}".format(i), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,9 @@ def islower(c):
     else:
         return False
 
+
 def uppercase(str):
     for c in str:
-        if(c.islower()):
-            print({":c"}.format(ord(c) - 32), end="")
+        if (c.islower()):
+            print("{:c}".format(ord(c) - 32), end="")
     print("")
