2ec2217160e8e5d0c86f6e836c778136bb89fe3d
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-12-11 20:56:31 +0200

    Updated

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    list_div = []
+    for i in my_list:
+        if i % 2 == 0:
+            list_div.append(True)
+        else:
+            list_div.append(False)
+    return (list_div)
