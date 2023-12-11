9b0e9bd7bb9d3f32e0c767397fee7b11d7f97049
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-12-11 18:44:14 +0200

    Updated

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    tuple_b= tuple_b + (0, 0)[:2 - len(tuple_b)]
+    tuple_a= tuple_a[:2]
+    tuple_b= tuple_b[:2]
+    return tuple(sum(x) for x in zip(tuple_a, tuple_b))
