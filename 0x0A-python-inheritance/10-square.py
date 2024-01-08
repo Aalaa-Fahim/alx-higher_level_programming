2eacf48b7b049d3f0dcbb46f84f9e4440d137342
Aalaa-Fahim <alaahussin.322001@gmail.com>
2024-01-09 00:11:00 +0200

    Updated

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
-Rectangle = __import__('9-rectangle').Rectangle
+"""Module for Rectangle class."""
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    '''A subclass representing a rectangle.'''
+    """A subclass representing a rectangle."""
     def __init__(self, size):
-        '''Constructor.'''
+        """A Constructor."""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        '''Method for the area of square.'''
+        """Method for the area of square."""
         return self.__size ** 2
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,17 +5,17 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """A Rectangle subclass"""
-     def __init__(self, width, height):
-         """A constructor"""
-         self.__width = width
-         self.__height = height
-         self.integer_validator("width", width)
-         self.integer_validator("height", height)
+    def __init__(self, width, height):
+        """A constructor"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
-     def area(self):
-         """A method to compute the area"""
-         return self.__width * self.__height
+    def area(self):
+        """A method to compute the area"""
+        return self.__width * self.__height
 
-     def str():
-         """A method to represent string"""
-         return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+    def str():
+        """A method to represent string"""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
